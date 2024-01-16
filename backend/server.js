ebb14003c0d47ac1183e5de7442bc4f72dceba05
Rorisang <roriiisang@gmail.com>
2024-01-16 12:14:59 +0200

    Added Dockerfile

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 
 // TODO: Integrate env variables into this connection string builder
 const pool = new Pool({
-  user: 'kbester',
+  user: 'rorisang123',
   host: 'the_db',
   database: 'grads',
   password: 'postgres',
